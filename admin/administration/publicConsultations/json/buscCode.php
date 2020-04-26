520fc5f216fc35ddf0f8795cb3e493a5461f4e46
Juan Manuel Gomez <juanma@erdos.local>
2020-04-26 17:44:18 -0500

    issue with DB query

--- admin/administration/publicConsultations/json/buscCode.php
+++ admin/administration/publicConsultations/json/buscCode.php
@@ -1,7 +1,7 @@
 <?php 
 	include_once '../../../../lib/j/j.func.php';
 
-	$stm = $db->prepare("SELECT COUNT(*) FROM publicConsultations WHERE code = ? AND id != ?");
+	$stm = $db->prepare("SELECT COUNT(*) FROM PublicConsultations WHERE code = ? AND id != ?");
 
 	$stm->execute(array($_POST['code'], $_POST['pcId']));
 	$cuenta = $stm -> fetch(PDO::FETCH_NUM);
