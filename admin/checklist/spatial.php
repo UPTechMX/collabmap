1e9f1095aeb23ba35ef32bb9872e59267e4f7e02
Juan Manuel Gomez <juanma@Hilbert.local>
2020-07-14 14:27:37 -0500

    change base map

--- admin/checklist/spatial.php
+++ admin/checklist/spatial.php
@@ -18,10 +18,10 @@
         // 	zoomOffset: -1
         // }).addTo(map);
 
-        L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+        L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png', {
         	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
         	subdomains: 'abcd',
-        	maxZoom: 19
+        	maxZoom: 30
         }).addTo(map);
 
 
